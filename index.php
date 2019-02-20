fc4ad4ea0d7397894c348cd16d4b6eabe49685d5
tatafella <47183467+tatafella@users.noreply.github.com>
2019-02-20 15:39:02 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "mysql";
+$username = "root";
+$password = "root";
+$dbname = "dockerbd";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT id, nom, race, description FROM personnage";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Nom: " . $row["nom"]. " - Prenom: " . $row["prenom"]. " - Adresse: " . $row["adresse"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
