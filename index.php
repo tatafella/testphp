8f9029337beaca87464ad371daee564d4838df67
tatafella <47183467+tatafella@users.noreply.github.com>
2019-02-20 16:21:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT id, nom, race, description FROM client";
+$sql = "SELECT id, nom, prenom, adresse FROM client";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
